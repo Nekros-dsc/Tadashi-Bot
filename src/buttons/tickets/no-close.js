43e64572c45a16601945343c25d5201ce99de046
!"Nekros <122643715+Nekros-dsc@users.noreply.github.com>
2023-04-29 22:30:39 +0200

    Delete src directory

--- src/buttons/tickets/no-close.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    customId: "no-close",
-    async execute(client, interaction) {
-        interaction.update({ content: "Chargement...", embeds: [], components: [] })
-        interaction.deleteReply()
-    }
-}
